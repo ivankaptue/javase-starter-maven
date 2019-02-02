c5e905d4e3c7ff18c9184e3359210ba005722b93
Ivan Kaptue <ivanokaptue@gmail.com>
2019-02-02 18:00:56 +0100

    initialization of project

---
+++ src/main/java/com/klid/App.java
@@ -0,0 +1,12 @@
+package com.klid;
+
+/**
+ * @author ivan.kaptue
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
